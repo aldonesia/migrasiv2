3d0b4f90bab083f7a0c024c56cc781531625de76
aldonesia <rizkyfenaldoo@gmail.com>
2017-07-28 15:22:34 +0700

    HDM

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -32,36 +32,6 @@ class Dashboard extends CI_Controller {
 		}
 	}
 
-	// public function WelcomePageAdmin() {
-	// 	$this->load->model('m_user');
-	// 	$data['title']				= 'TMM';
-	// 	$data['page_header']		= 'Telkom Migration Monitoring';
-	// 	$data['username_logged']	= $this->session->userdata('username');
-	// 	$data['role']				= $this->session->userdata('role');
-	// 	$data['nama']				= $this->session->userdata('nama');
-	// 	if($data['role'] == 'RO2'){
-	// 		$this->load->view('design/admin/header',$data);
-	// 		$this->load->view('admin/welcome',$data);
-	// 		$this->load->view('design/admin/footer',$data);
-	// 	}
-	// 	else redirect('dashboard');
-	// }
-
-	// public function WelcomePageHDTA() {
-	// 	$this->load->model('m_user');
-	// 	$data['title']				= 'TMM';
-	// 	$data['page_header']		= 'Telkom Migration Monitoring';
-	// 	$data['username_logged']	= $this->session->userdata('username');
-	// 	$data['role']				= $this->session->userdata('role');
-	// 	$data['nama']				= $this->session->userdata('nama');
-	// 	if($data['role'] == 'RO4'){
-	// 		$this->load->view('design/hdta/header',$data);
-	// 		$this->load->view('HDTA/welcome',$data);
-	// 		$this->load->view('design/hdta/header',$data);
-	// 	}
-	// 	else redirect('dashboard');
-	// }
-
 	public function WelcomePageHDM() {
 		$this->load->model('m_user');
 		$data['page_header']		= 'Telkom Migration Monitoring';
@@ -76,49 +46,9 @@ class Dashboard extends CI_Controller {
 		}
 		$data['namamitra'] = $temp;
 		if($data['role'] == 'RO7'){
-			// $this->load->view('Design/header');
-			// $this->load->view('HDM/sidebar',$data)
-			// $this->load->view('HDM/checkwo',$data);
-			// $this->load->view('Design/footer');
-			redirect('HDM/CheckWoHDM');
+			redirect('HDM');
 		}
 		else redirect('Dashboard');
 	}
 
-	// public function WelcomePageTeknisi() {
-	// 	$this->load->model('m_user');
-	// 	$data['title']				= 'TMM';
-	// 	$data['page_header']		= 'Telkom Migration Monitoring';
-	// 	$data['username_logged']	= $this->session->userdata('username');
-	// 	$data['role']				= $this->session->userdata('role');
-	// 	$data['nama']				= $this->session->userdata('nama');
-	// 	$query	= $this->m_user->check_mitra($this->session->userdata('mitra'));
-	// 	foreach ($query as $object) {
-	// 		if($object->id_mitra == $this->session->userdata('mitra')) {
-	// 			$temp 	= $object->nama_mitra;
-	// 		}
-	// 	}
-	// 	$data['namamitra'] = $temp;
-	// 	if($data['role'] == 'RO6'){
-	// 		 $this->load->view('design/teknisi/header',$data);
-	// 		 $this->load->view('teknisi/welcome',$data);
-	// 		 $this->load->view('design/teknisi/footer',$data);
-	// 	}
-	// 	else redirect('dashboard');
-	// }
-
-	// public function WelcomePageSuperUser() {
-	// 	$this->load->model('m_user');
-	// 	$data['title']				= 'TMM';
-	// 	$data['page_header']		= 'Telkom Migration Monitoring';
-	// 	$data['username_logged']	= $this->session->userdata('username');
-	// 	$data['role']				= $this->session->userdata('role');
-	// 	$data['nama']				= $this->session->userdata('nama');
-	// 	if($data['role'] == 'RO1') {
-	// 		$this->load->view('design/admin/header',$data);
-	// 		$this->load->view('superuser/welcome',$data);
-	// 		$this->load->view('design/admin/footer',$data);
-	// 	}
-	// 	else redirect('dashboard');
-	// }
 }
--- application/controllers/HDM.php
+++ application/controllers/HDM.php
@@ -1,59 +1,201 @@
 <?php if ( ! defined('BASEPATH')) exit ('No direct script access allowed');
 
-class WorkOrder extends CI_Controller {	
+class HDM extends CI_Controller {	
 	public function __construct() {
         parent::__construct();
         $this->load->model('m_wo');
-        // Your own constructor code
+        $this->load->model('m_user');
+        $this->load->model('m_log');
     }
 
 	public function index() {
 		$is_logged_in = $this->session->userdata('is_logged_in');
 		if($is_logged_in) {
-			redirect('Dashboard');
+			$data['page_header'] = 'Checking WO . . .';
+			$mitra = $this->session->userdata('mitra');
+			$query = $this->m_wo->get_all_teknisi($mitra);
+			$data['select_teknisi'] = array();
+			foreach ($query as $object) {
+				$data['select_teknisi'][$object->id_user]=$object->nama_user;
+			}
+			$this->load->view('Design/header',$data);
+			$this->load->view('HDM/checkwoHDM',$data);
+			$this->load->view('Design/footer');
 		}
 		else {
 			redirect('Access');
 		}
 	}
 
-	public function CheckWoHDM(){
-		$updatedata['flag'] = FALSE;
-		$this->load->model('m_user');
-		$data['title'] = 'TMM';
-		$data['page_header'] = 'Checking WO . . .';
-		$data['mitra'] = $this->session->userdata('mitra');
-		$data['username_logged'] = $this->session->userdata('username');
-		$query 	= $this->m_wo->checkwo($data['mitra']);
-		if($query) {
-			$data['flag'] = TRUE;
-			$data['query'] = $query;
-		}
-		else $data['flag'] = FALSE;
-		$query_fase		= $this->m_user->get_all_fase();
-		$query_status	= $this->m_user->get_all_status();
-		$query_teknisi	= $this->m_user->get_all_teknisi($data['mitra']);
-		$data['query_fase'] = $query_fase;
-		$data['query_teknisi'] = $query_teknisi;
-		$data['query_status'] = $query_status;
-		$data['select'] = Array();
-		if($data['query_teknisi']) {
-			foreach($query_teknisi as $r) {
-    		$data['select'][$r->id_user] = $r->nama_user;
-			}
-		}
-		$this->load->view('Design/header',$data);
-		$this->load->view('HDM/checkwoHDM',$data);
-		$this->load->view('Design/footer',$data);
+	public function ajax_list()
+    {
+    	$mitra = $this->session->userdata('mitra');
+    	$fase = $this->m_wo->get_all_fase();
+    	$nama_teknisi = $this->m_wo->get_nama_teknisi();
+        $list = $this->m_wo->get_datatables($mitra);
+        $data = array();
+        $no = $_POST['start'];
+        foreach ($list as $wo) {
+            $no++;
+            $row = array();
+            $row[] = $wo->TGL_DATA_MASUK;
+            foreach($fase as $object) {
+                if($object->id_fase == $wo->FASE_TRANSAKSI) $row[] = $object->nama_fase; 
+            }
+            $row[] = $wo->KETERANGAN_TAMBAHAN;
+            $row[] = $wo->TGL_INPUT_TEKNISI;
+            if ($wo->ID_TEKNISI != NULL)
+            {
+	            foreach($nama_teknisi as $object) {
+	                if($object->id_user == $wo->ID_TEKNISI) $row[] = $object->nama_user;
+	            }
+	        }
+	        else{
+	        	$row[] = $wo->ID_TEKNISI;
+	        }
+            $row[] = $wo->ND;
+            $row[] = $wo->NAMA_PELANGGAN;
+            $row[] = $wo->STO;
+            $row[] = $wo->ODP;
+            $row[] = $wo->SN;
+ 
+            //add html for action
+            if($wo->ID_TEKNISI != NULL)
+            {
+            	$row[] = '<a class="btn btn-sm btn-primary" href="javascript:void(0)" title="AddSNODP" onclick="add_sn_odp('."'".$wo->ID_TRANSAKSI."'".')"><i class="glyphicon glyphicon-pencil"></i> Tambah SN ODP</a>
+                  <a class="btn btn-sm btn-primary" href="javascript:void(0)" title="AddKeterangan" onclick="add_keterangan('."'".$wo->ID_TRANSAKSI."'".')"><i class="glyphicon glyphicon-pencil"></i> Tambah Keterangan</a>';
+            }
+            else
+            {
+            	$row[] = '<a class="btn btn-sm btn-danger" href="javascript:void(0)" title="AddTeknisi" onclick="add_teknisi('."'".$wo->ID_TRANSAKSI."'".')"><i class="glyphicon glyphicon-pencil"></i> Tambah Teknisi</a>
+                  <a class="btn btn-sm btn-primary" href="javascript:void(0)" title="AddSNODP" onclick="add_sn_odp('."'".$wo->ID_TRANSAKSI."'".')"><i class="glyphicon glyphicon-pencil"></i> Tambah SN ODP</a>
+                  <a class="btn btn-sm btn-primary" href="javascript:void(0)" title="AddKeterangan" onclick="add_keterangan('."'".$wo->ID_TRANSAKSI."'".')"><i class="glyphicon glyphicon-pencil"></i> Tambah Keterangan</a>';
+            }
+            
+         
+            $data[] = $row;
+        }
+ 
+        $output = array(
+                        "draw" => $_POST['draw'],
+                        "recordsTotal" => $this->m_wo->count_all(),
+                        "recordsFiltered" => $this->m_wo->count_filtered($mitra),
+                        "data" => $data,
+                );
+        //output to json format
+        echo json_encode($output);
+    }
+ 
+    public function ajax_edit($id_transaksi)
+    {
+        $data = $this->m_wo->get_by_id_transaksi($id_transaksi);
+        echo json_encode($data);
+    }
+ 
+    public function ajax_add_teknisi()
+    {
+        //$this->_validate();
+        $nd = $this->input->post('ND');
+        $data = array(
+                'ID_TEKNISI' => $this->input->post('idteknisi'),
+                'TGL_INPUT_TEKNISI' => date('Y-m-d'),
+                'FASE_TRANSAKSI' => 'FA03',
+                'KETERANGAN_TAMBAHAN' => 'Belum Disurvey'
+            );
+        $status = $this->m_log->getstatus();
+        foreach($status as $object_status) {
+        	if($nd == $object_status->ND) $id_status = $object_status->STATUS;
+        }
+        $fase = $this->m_log->getfase();
+        foreach($fase as $object_fase) {
+        	if($nd == $object_fase->ND) $id_fase = $object_fase->FASE_TRANSAKSI;
+        }
+        $keterangan = $this->m_log->getketerangan();
+        foreach($keterangan as $object_keterangan) {
+        	if($nd == $object_keterangan->ND) $keterangan = $object_keterangan->KETERANGAN_TAMBAHAN;
+        }
+        
+        $log = array(
+        		'id_log' => NULL,
+        		'tanggal_log' => date('Y-m-d'),
+        		'ND_log' => $nd,
+        		'id_fase_log' => $id_fase,
+        		'id_status_log' => $id_status,
+        		'keterangan_log' => $keterangan,
+        		'action_log' => 'ASSIGN TEKNISI',
+        		'updated_by_log'=> $mitra = $this->session->userdata('nama')
+        	);
+        $this->m_wo->update(array('ND'=> $this->input->post('ND')), $data);
+        $this->m_log->insertlog($log);	
+        echo json_encode(array("status" => TRUE));	
+        
+    }
 
-		$updatedata = array(
-			'id' => $this->input->post('idt'),
-			'tek' => $this->input->post('teknisi'),
-			'flag' => $this->input->post('flag')
-		);
-		$temp = $this->m_wo->assign_teknisi($updatedata);
-		if($updatedata['flag']) {
-			redirect('HDM/CheckwoHDM','refresh');
-		}
-	}
+    public function ajax_add_sn_odp()
+    {
+        //$this->_validate();
+        $nd = $this->input->post('ND');
+        $data = array(
+                'SN' => $this->input->post('SN'),
+                'ODP' => $this->input->post('ODP'),
+            );
+        
+        $status = $this->m_log->getstatus();
+        foreach($status as $object_status) {
+        	if($nd == $object_status->ND) $id_status = $object_status->STATUS;
+        }
+        $fase = $this->m_log->getfase();
+        foreach($fase as $object_fase) {
+        	if($nd == $object_fase->ND) $id_fase = $object_fase->FASE_TRANSAKSI;
+        }
+        $keterangan = $this->m_log->getketerangan();
+        foreach($keterangan as $object_keterangan) {
+        	if($nd == $object_keterangan->ND) $keterangan = $object_keterangan->KETERANGAN_TAMBAHAN;
+        }
+        
+        $log = array(
+        		'id_log' => NULL,
+        		'tanggal_log' => date('Y-m-d'),
+        		'ND_log' => $nd,
+        		'id_fase_log' => $id_fase,
+        		'id_status_log' => $id_status,
+        		'keterangan_log' => $keterangan,
+        		'action_log' => 'TAMBAH SN DAN ODP',
+        		'updated_by_log'=> $mitra = $this->session->userdata('nama')
+        	);
+        $this->m_wo->update(array('ND'=> $this->input->post('ND')), $data);
+        $this->m_log->insertlog($log);
+        echo json_encode(array("status" => TRUE));
+    }
+ 
+ 	public function ajax_add_keterangan()
+    {
+        $nd = $this->input->post('ND');
+        $data = array(
+                'KETERANGAN_TAMBAHAN' => $this->input->post('Keterangan'),
+        );
+        $status = $this->m_log->getstatus();
+        foreach($status as $object_status) {
+        	if($nd == $object_status->ND) $id_status = $object_status->STATUS;
+        }
+        $fase = $this->m_log->getfase();
+        foreach($fase as $object_fase) {
+        	if($nd == $object_fase->ND) $id_fase = $object_fase->FASE_TRANSAKSI;
+        }
+        
+        $log = array(
+        		'id_log' => NULL,
+        		'tanggal_log' => date('Y-m-d'),
+        		'ND_log' => $nd,
+        		'id_fase_log' => $id_fase,
+        		'id_status_log' => $id_status,
+        		'keterangan_log' => $data['KETERANGAN_TAMBAHAN'],
+        		'action_log' => 'TAMBAH SN DAN ODP',
+        		'updated_by_log'=> $mitra = $this->session->userdata('nama')
+        	);
+        $this->m_wo->update(array('ND'=> $this->input->post('ND')), $data);
+        $this->m_log->insertlog($log);
+        echo json_encode(array("status" => TRUE));	
+        
+    }
 }
